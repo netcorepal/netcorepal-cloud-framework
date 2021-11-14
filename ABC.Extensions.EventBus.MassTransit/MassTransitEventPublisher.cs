9bc04ac46c51ca055866f9d193d9d072b44a7361
weiyu.xiao <weiyu.xiao@outlook.com>
2021-11-14 13:10:58 +0800

    添加eventbus相关接口类

---
+++ ABC.Extensions.EventBus.MassTransit/MassTransitEventPublisher.cs
@@ -0,0 +1,20 @@
+﻿using ABC.Extensions.EventBus;
+using MassTransit;
+
+namespace ABC.Extensions.EventBus.MassTransit
+{
+    public class MassTransitEventPublisher : IEventPublisher
+    {
+        IPublishEndpoint _publishEndpoint;
+        public MassTransitEventPublisher(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+
+        public Task PublishAsync<T>(string eventName, T eventData, CancellationToken cancellationToken = default) where T : class
+        {
+            return _publishEndpoint.Publish<T>(eventData, cancellationToken);
+        }
+    }
+}
