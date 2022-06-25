3485a89bf77936eb84ddd70131236195e0e54ebb
weiyu.xiao <weiyu.xiao@outlook.com>
2022-06-25 10:28:58 +0800

    添加依赖注入

---
+++ src/ServiceDiscovery.Client/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NetCorePal.ServiceDiscovery;
+using NetCorePal.ServiceDiscovery.Client;
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddNetCorePalServiceDiscoveryClient(this IServiceCollection services)
+        {
+            services.AddSingleton<IServiceDiscoveryClient, DefaultServiceDiscoveryClient>();
+            return services;
+        }
+    }
+}
+
