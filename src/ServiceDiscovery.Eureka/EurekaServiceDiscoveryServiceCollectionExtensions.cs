34526ebadf84c70852cdc3fa555c9f9716954aca
weiyu.xiao <weiyu.xiao@outlook.com>
2022-06-26 16:15:04 +0800

    添加EurekaServiceDiscoveryServiceCollectionExtensions

---
+++ src/ServiceDiscovery.Eureka/EurekaServiceDiscoveryServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using NetCorePal.ServiceDiscovery;
+using NetCorePal.ServiceDiscovery.Eureka;
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class EurekaServiceDiscoveryServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEurekaServiceDiscovery(this IServiceCollection services)
+        {
+            services.AddSingleton<IServiceDiscoveryProvider, EurekaServiceDiscoveryProvider>();
+            return services;
+        }
+    }
+}
--- src/ServiceDiscovery.K8S/K8SServiceDiscoveryServiceCollectionExtensions.cs
+++ src/ServiceDiscovery.K8S/K8SServiceDiscoveryServiceCollectionExtensions.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using NetCorePal.ServiceDiscovery.K8S;
 using NetCorePal.ServiceDiscovery;
 
-namespace NetCorePal.ServiceDiscovery.K8S
+namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class ServiceDiscoveryServiceCollectionExtensions
+    public static class K8SServiceDiscoveryServiceCollectionExtensions
     {
         /// <summary>
         /// 添加服务发现
