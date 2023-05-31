1c86791c6acfcb6fbe4369ee5e8df9cb5b78b89a
witskeeper <witskeeper@gmail.com>
2023-05-31 22:01:01 +0800

    添加ICommand接口

---
+++ src/Primitives/ICommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+namespace NetCorePal.Extensions.Primitives
+{
+    public interface ICommand : IRequest { }
+
+
+    public interface ICommand<out TResponse> : IRequest<TResponse> { }
+}
---
+++ src/Primitives/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+namespace NetCorePal.Extensions.Primitives
+{
+    public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand> where TCommand : ICommand
+    {
+    }
+
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse> where TCommand : ICommand<TResponse>
+    {
+    }
+}
