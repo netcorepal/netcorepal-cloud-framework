916bcd8465a80f4dfec37cdc496d562e3ffe4f9e
witskeeper <witskeeper@gmail.com>
2023-06-13 23:15:14 +0800

    源生成器

---
+++ test/ConsoleApp/OrderId.cs
@@ -0,0 +1,12 @@
+﻿using NetCorePal.Extensions.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public record OrderId(long Id) : IEntityId { }
+    public record OrderId2(int Id) : IEntityId { }
+}
