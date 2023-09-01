636987d95bb71072e0ee2cea04cda45c9884d69a
witskeeper <witskeeper@gmail.com>
2023-09-02 02:18:07 +0800

    add EFContextSourceGenerator

---
+++ test/ConsoleApp/AbcContext.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NetCorePal.Extensions.Repository.EntityframeworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCorePal.ConsoleApp
+{
+    public partial class AbcContext : EFContext
+    {
+        public AbcContext(DbContextOptions options, IMediator mediator, IServiceProvider provider) : base(options, mediator, provider)
+        {
+        }
+    }
+}
