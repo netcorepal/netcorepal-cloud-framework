1807ca17d730c6779672c9021dd0a98774b88ad8
witskeeper <witskeeper@gmail.com>
2023-10-14 09:00:06 +0800

    remove eureka provider

--- test/NetCorePal.Extensions.ServiceDiscovery.Eureka.UnitTests/Usings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Xunit;
-global using Microsoft.Extensions.Options;
-global using Microsoft.Extensions.DependencyInjection;
-global using NetCorePal.Extensions.ServiceDiscovery;
-global using NetCorePal.Extensions.ServiceDiscovery.Eureka;
-global using Microsoft.Extensions.Logging;
