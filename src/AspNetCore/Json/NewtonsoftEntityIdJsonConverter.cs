dbef75ceaead31d6c0e34bcc6dd544e73913c022
witskeeper <witskeeper@gmail.com>
2023-10-27 13:37:54 +0800

    add NewtonsoftEntityIdJsonConverter

---
+++ src/AspNetCore/Json/NewtonsoftEntityIdJsonConverter.cs
@@ -0,0 +1,71 @@
+using System.Reflection;
+using NetCorePal.Extensions.Domain;
+using Newtonsoft.Json;
+
+namespace NetCorePal.Extensions.AspNetCore.Json;
+
+public class NewtonsoftEntityIdJsonConverter : JsonConverter
+{
+    readonly Dictionary<Type, ConstructorInfo> _ConstructorInfo = new();
+
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        if (value is null)
+            writer.WriteNull();
+        else
+            writer.WriteValue(value.ToString());
+    }
+
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue,
+        JsonSerializer serializer)
+    {
+        object? v = default;
+        if (reader.TokenType == JsonToken.String)
+        {
+            var value = reader.Value?.ToString();
+
+            if (value != null)
+            {
+                if (!_ConstructorInfo.TryGetValue(objectType, out var _constructorInfo))
+                {
+                    _constructorInfo = objectType.GetConstructors()[0];
+                    _ConstructorInfo.Add(objectType, _constructorInfo);
+                }
+
+                Type parameterType = _constructorInfo.GetParameters()[0].ParameterType;
+
+                if (parameterType == typeof(long))
+                {
+                    v = _constructorInfo.Invoke(new object[] { long.Parse(value) });
+                }
+                else if (parameterType == typeof(int))
+                {
+                    v = _constructorInfo.Invoke(new object[] { int.Parse(value) });
+                }
+                else if (parameterType == typeof(string))
+                {
+                    v = _constructorInfo.Invoke(new object[] { value });
+                }
+                else if (parameterType == typeof(Guid))
+                {
+                    v = _constructorInfo.Invoke(new object[] { Guid.Parse(value) });
+                }
+            }
+        }
+
+        return v;
+    }
+
+    Type[] _supportedTypes = new[]
+    {
+        typeof(IInt64StronglyTypedId),
+        typeof(IInt32StronglyTypedId),
+        typeof(IGuidStronglyTypedId),
+        typeof(IStringStronglyTypedId),
+    };
+
+    public override bool CanConvert(Type objectType)
+    {
+        return _supportedTypes.Any(p => p.IsAssignableFrom(objectType));
+    }
+}
---
+++ test/NetCorePal.Extensions.AspNetCore.UnitTests/NewtonsoftEntityIdJsonConverterTests.cs
@@ -0,0 +1,92 @@
+using NetCorePal.Extensions.AspNetCore.Json;
+using NetCorePal.Extensions.Domain;
+using Newtonsoft.Json;
+
+namespace NetCorePal.Extensions.AspNetCore.UnitTests;
+
+public partial record MyLongId : IInt64StronglyTypedId;
+
+public partial record MyIntId : IInt32StronglyTypedId;
+
+public partial record MyGuidId : IGuidStronglyTypedId;
+
+public partial record MyStringId : IStringStronglyTypedId;
+
+public class NewtonsoftEntityIdJsonConverterTests
+{
+    [Fact]
+    public void CanConvertTest()
+    {
+        var converter = new NewtonsoftEntityIdJsonConverter();
+
+        Assert.True(converter.CanConvert(typeof(MyLongId)));
+        Assert.True(converter.CanConvert(typeof(MyIntId)));
+        Assert.True(converter.CanConvert(typeof(MyGuidId)));
+        Assert.True(converter.CanConvert(typeof(MyStringId)));
+        Assert.False(converter.CanConvert(typeof(object)));
+    }
+
+    [Fact]
+    public void Int64EntityTest()
+    {
+        JsonSerializerSettings settings = new();
+        settings.Converters.Add(new NewtonsoftEntityIdJsonConverter());
+        var id = new MyLongId(1);
+        var json = JsonConvert.SerializeObject(id, settings);
+        Assert.Equal("\"1\"", json);
+        var id2 = JsonConvert.DeserializeObject<MyLongId>(json, settings);
+        Assert.Equal(id, id2);
+        var id3 = JsonConvert.DeserializeObject<MyLongId>("null", settings);
+        Assert.Null(id3);
+
+        Assert.Throws<FormatException>(() => JsonConvert.DeserializeObject<MyLongId>("\"\"", settings));
+    }
+
+    [Fact]
+    public void Int32EntityTest()
+    {
+        JsonSerializerSettings settings = new();
+        settings.Converters.Add(new NewtonsoftEntityIdJsonConverter());
+        var id = new MyIntId(1);
+        var json = JsonConvert.SerializeObject(id, settings);
+        Assert.Equal("\"1\"", json);
+        var id2 = JsonConvert.DeserializeObject<MyIntId>(json, settings);
+        Assert.Equal(id, id2);
+        var id3 = JsonConvert.DeserializeObject<MyIntId>("null", settings);
+        Assert.Null(id3);
+
+        Assert.Throws<FormatException>(() => JsonConvert.DeserializeObject<MyIntId>("\"\"", settings));
+    }
+
+    [Fact]
+    public void GuidEntityTest()
+    {
+        JsonSerializerSettings settings = new();
+        settings.Converters.Add(new NewtonsoftEntityIdJsonConverter());
+        var id = new MyGuidId(Guid.NewGuid());
+        var json = JsonConvert.SerializeObject(id, settings);
+        Assert.Equal("\"" + id.Id.ToString() + "\"", json);
+        var id2 = JsonConvert.DeserializeObject<MyGuidId>(json, settings);
+        Assert.Equal(id, id2);
+        var id3 = JsonConvert.DeserializeObject<MyGuidId>("null", settings);
+        Assert.Null(id3);
+
+        Assert.Throws<FormatException>(() => JsonConvert.DeserializeObject<MyGuidId>("\"\"", settings));
+    }
+
+    [Fact]
+    public void StringEntityTest()
+    {
+        JsonSerializerSettings settings = new();
+        settings.Converters.Add(new NewtonsoftEntityIdJsonConverter());
+        var id = new MyStringId("1");
+        var json = JsonConvert.SerializeObject(id, settings);
+        Assert.Equal("\"1\"", json);
+        var id2 = JsonConvert.DeserializeObject<MyStringId>(json, settings);
+        Assert.Equal(id, id2);
+        var id3 = JsonConvert.DeserializeObject<MyStringId>("null", settings);
+        Assert.Null(id3);
+        var id4 = JsonConvert.DeserializeObject<MyStringId>("\"\"", settings);
+        Assert.Equal("", id4.Id);
+    }
+}
