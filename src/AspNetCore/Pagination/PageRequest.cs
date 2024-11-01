63e9b6f05b24aa6aa73a8ee78a3474476760d8ef
frogleyzhoud <frogleyzhoud@163.com>
2024-11-01 17:27:57 +0800

    1. Added support for pagination functionality 添加对分页的支持

---
+++ src/AspNetCore/Pagination/PageRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace NetCorePal.Extensions.AspNetCore.Pagination;
+
+public class PageRequest
+{
+    public int? Index { get; set; }
+    public int? Size { get; set; }
+}
---
+++ src/AspNetCore/Pagination/PagedData.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCorePal.Extensions.AspNetCore.Pagination;
+
+public class PagedData<T>(IEnumerable<T> items, int total, int page, int size)
+{
+    public IEnumerable<T> Items { get; set; } = items;
+    public int Total { get; set; } = total;
+    public int Index { get; set; } = page;
+    public int Size { get; set; } = size;
+}
