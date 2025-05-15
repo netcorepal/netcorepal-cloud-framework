ffed61799407a951d1dc101e8a8cd90a7915bb99
yunus0712 <2794914797@qq.com>
2025-05-15 09:15:22 +0800

    fix: 优化单元测试

--- test/NetCorePal.Extensions.Dto.Tests/IQueryableExtensionsTests.cs
+++ test/NetCorePal.Extensions.Dto.Tests/IQueryableExtensionsTests.cs
@@ -55,8 +55,8 @@ public class IQueryableExtensionsTests
     [Fact]
     public void ThenByIfTests()
     {
-        var data = new List<int> { 2, 5, 1, 4, 3 }.AsQueryable();
-        var result1 = data.OrderBy(x => 0).ThenByIf(condition: true, predicate: x => x, desc: false).ToList();
+        var data = new List<int> { 2, 5, 1, 4, 3 }.AsQueryable().OrderBy(x => 0);
+        var result1 = data.ThenByIf(condition: true, predicate: x => x, desc: false).ToList();
         Assert.Equal(5, result1.Count);
         Assert.Equal(1, result1[0]);
         Assert.Equal(2, result1[1]);
@@ -64,7 +64,7 @@ public class IQueryableExtensionsTests
         Assert.Equal(4, result1[3]);
         Assert.Equal(5, result1[4]);
 
-        var result2 = data.OrderBy(x => 0).ThenByIf(condition: true, predicate: x => x, desc: true).ToList();
+        var result2 = data.ThenByIf(condition: true, predicate: x => x, desc: true).ToList();
         Assert.Equal(5, result2.Count);
         Assert.Equal(5, result2[0]);
         Assert.Equal(4, result2[1]);
@@ -72,7 +72,7 @@ public class IQueryableExtensionsTests
         Assert.Equal(2, result2[3]);
         Assert.Equal(1, result2[4]);
 
-        var result3 = data.OrderBy(x => 0).ThenByIf(condition: false, predicate: x => x, desc: false).ToList();
+        var result3 = data.ThenByIf(condition: false, predicate: x => x, desc: false).ToList();
         Assert.Equal(5, result3.Count);
         Assert.Equal(2, result3[0]);
         Assert.Equal(5, result3[1]);
@@ -80,7 +80,7 @@ public class IQueryableExtensionsTests
         Assert.Equal(4, result3[3]);
         Assert.Equal(3, result3[4]);
 
-        var result4 = data.OrderBy(x => 0).ThenByIf(condition: false, predicate: x => x, desc: true).ToList();
+        var result4 = data.ThenByIf(condition: false, predicate: x => x, desc: true).ToList();
         Assert.Equal(5, result4.Count);
         Assert.Equal(2, result4[0]);
         Assert.Equal(5, result4[1]);
